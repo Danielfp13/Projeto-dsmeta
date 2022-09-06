15fc054d95204a217f38e3200680d01b85d49bb6
Daniel Pereira <daniel.si@outlook.com.br>
2022-09-06 15:20:18 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import logo from '../../assets/img/logo.svg';
+import './styles.css';
+
+function Header() {
+    return (
+        <>
+            <div className="dsmeta-logo-container">
+                <img src= {logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.instagram.com/devsuperior.ig"> Daniel F Pereira</a>
+                </p>
+            </div>
+
+
+        </>
+
+    )
+}
+
+export default Header;
